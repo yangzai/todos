8103946ee6802060d203e1a681b8ee2be9dbd38c
Ang Hao Yang <yan9za1@gmail.com>
2014-10-24 04:44:46 +0800

    updated schema.

--- schema.js
+++ schema.js
@@ -5,10 +5,18 @@ var pg = require('pg')
 
 client = new pg.Client(connectionString);
 client.connect();
-query = client.query('CREATE TABLE todos (\
-                    name TEXT NOT NULL, \
-                    todo TEXT NOT NULL, \
-                    PRIMARY KEY(name, todo)\
-                    );'
-);
+query = client.query('DROP TABLE users; DROP TABLE todos; \
+                     CREATE TABLE users (\
+                        id SERIAL PRIMARY KEY, \
+                        name TEXT NOT NULL UNIQUE\
+                    ); \
+                    CREATE TABLE todos (\
+                        user_id SERIAL, \
+                        todo TEXT NOT NULL, \
+                        timestamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP, \
+                        PRIMARY KEY(user_id, todo), \
+                        FOREIGN KEY(user_id) REFERENCES users(id) \
+	                       ON UPDATE CASCADE \
+	                       ON DELETE RESTRICT \
+                    );');
 query.on('end', function() { client.end(); });
