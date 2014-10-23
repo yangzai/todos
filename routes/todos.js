5de6ca583c79138210c27d3512aac40f015d5fc5
Ang Hao Yang <yan9za1@gmail.com>
2014-10-24 04:36:37 +0800

    basic completed. before wrapper.

--- routes/todos.js
+++ routes/todos.js
@@ -4,12 +4,6 @@ var router = express.Router();
 var pg = require('pg');
 var conString;
 
-/* GET users listing. */
-//router.get('/', function(req, res) {
-//  //res.send('respond with a resource');
-//    res.json({todo: "feed the cat.", name: "bob"});
-//});
-
 var env = process.env.NODE_ENV || 'development';
 if ('development' === env) {
     conString = 'postgres://yangzai@localhost/tododb'
@@ -17,110 +11,141 @@ if ('development' === env) {
     conString = process.env.DATABASE_URL
 }
 
+
 router.route('/')
 .get(function (req, res) {
-    
-    //db.connect(function(err) {
     pg.connect(conString, function(err, client, done) {
         if(err) {
-            return console.error('could not connect to postgres', err);
+            console.error('could not connect to postgres', err);
+            return res.status(500).end();
         }
-        client.query('SELECT * FROM todos',  function(err, result) {
+        client.query('SELECT name, todo FROM users, todos WHERE id = user_id ORDER BY timestamp DESC;',  function(err, result) {
             if(err) {
-                return console.error('error running query', err);
+                console.error('error running query', err);
+                return res.status(500).end();
             }
-            //console.log(result.rows[0].theTime);
+
             var body = []
             result.rows.forEach(function (element) {
                 body.push({todo: element.todo, name: element.name})
                 
             });
-            //res.send(body)
-            console.dir(body)
+
             res.json(body);
-            //res.json(result.rows[0].name)
-            //output: Tue Jan 15 2013 19:12:47 GMT-600 (CST)
-            console.dir("in")
             done();
-            //db.end();
         });
     });
-    //console.dir(body);
-    //res.json(body);
 })
 .post(function (req, res) { // insertion duplicates?
-    console.dir(req.body);
-    //db.connect(function(err) {
     pg.connect(conString, function(err, client, done) {
         if(err) {
-            return console.error('could not connect to postgres', err);
+            console.error('could not connect to postgres', err);
+            return res.status(500).end();
+        }    
+        if (!req.body.name || !req.body.todo) {
+            return res.status(400).send('Malform request.');
         }
-        client.query('INSERT INTO todos VALUES($1, $2)', [req.body.name, req.body.todo], function(err, result) {
+        
+        client.query('SELECT id from users WHERE name = $1;', [req.body.name], function (err, result) {
             if(err) {
-                //db.end();
-                return console.error('error running query', err);
+                console.error('error running query', err);
+                return res.status(500).end();
             }
-            //console.log(result.rows[0].theTime);
-            //output: Tue Jan 15 2013 19:12:47 GMT-600 (CST)
-            console.dir("in")
-            //db.end();
-            done();
+
+            if (!result.rows.length) {
+                client.query('INSERT INTO users VALUES(DEFAULT, $1) RETURNING id;', [req.body.name], function(err, result) {
+                    if(err) {
+                        console.error('error running query', err);
+                        return res.status(500).end();
+                    }
+                    pgInsertIntoTodoAndFinish(client, done, res, result.rows[0].id, req.body.todo);
+                });
+            } else {
+                pgInsertIntoTodoAndFinish(client, done, res, result.rows[0].id, req.body.todo);
+            }            
         });
     });
-
-    res.send(req.body);
 })
 .delete(function (req, res) { // task not exist?
     pg.connect(conString, function(err, client, done) {
         if(err) {
-            return console.error('could not connect to postgres', err);
+            console.error('could not connect to postgres', err);
+            return res.status(500).end();
+        }    
+        if (!req.body.name || !req.body.todo) {
+            return res.status(400).send('Malform request.');
         }
-        client.query('DELETE FROM todos WHERE name = $1 AND todo = $2', [req.body.name, req.body.todo], function(err, result) {
+        
+        client.query('SELECT name, todo FROM users, todos WHERE name = $1;', [req.body.name], function (err, result) {
             if(err) {
-                //db.end();
-                return console.error('error running query', err);
+                console.error('error running query', err);
+                return res.status(500).end();
             }
-            //console.log(result.rows[0].theTime);
-            //output: Tue Jan 15 2013 19:12:47 GMT-600 (CST)
-            console.dir("in")
-            //db.end();
-            done();
+
+            if (!result.rows.length) {
+                client.query('INSERT INTO users VALUES(DEFAULT, $1) RETURNING id;', [req.body.name], function(err, result) {
+                    if(err) {
+                        console.error('error running query', err);
+                        return res.status(500).end();
+                    }
+                    pgDeleteFromTodoAndFinish(client, done, result.rows[0].id, req.body.todo);
+                });
+            } else {
+                pgDeleteFromTodoAndFinish(client, done, result.rows[0].id, req.body.todo);
+            }            
         });
     });
-
-    res.send(req.body);
 });
 
-router.route('/:user')
+router.route('/:user') //name not id
 .get(function (req, res) {
         pg.connect(conString, function(err, client, done) {
         if(err) {
-            return console.error('could not connect to postgres', err);
-        }
-        client.query('SELECT * FROM todos', [req.params.user],  function(err, result) {
+            console.error('could not connect to postgres', err);
+            return res.status(500).end();
+        } 
+        client.query('SELECT name, todo FROM users, todos WHERE name = $1 AND id = user_id ORDER BY timestamp DESC;'
+                     , [req.params.user],  function(err, result) {
             if(err) {
-                return console.error('error running query', err);
+                console.error('error running query', err);
+                return res.status(500).end();
             }
-            //console.log(result.rows[0].theTime);
             var body = []
             result.rows.forEach(function (element) {
                 body.push({todo: element.todo, name: element.name})
                 
             });
-            //res.send(body)
-            console.dir(req.params.user)
+
             res.json(body);
-            //res.json(result.rows[0].name)
-            //output: Tue Jan 15 2013 19:12:47 GMT-600 (CST)
-            console.dir("in")
             done();
-            //db.end();
         });
     });
-    //res.json({todo: "feed the cat.", name: "bob"});
 });
-//.post(function (req, res) {
-//    console.dir(req.body);
-//    res.send(req.body);
-//});
+
 module.exports = router;
+
+//
+function pgInsertIntoTodoAndFinish(client, done, res, userId, todo) {
+    client.query('INSERT INTO todos VALUES($1, $2)', [userId, todo], function(err, result) {
+        if(err) {
+            console.error('error running query', err);
+            
+            if (err.code == 23505) //unique violation
+                return res.status(400).send('Task already exist.');
+
+            return res.status(500).end();
+        }
+        res.status(200).end();
+        done();
+    });
+}
+
+function pgDeleteFromTodoAndFinish(client, done, res, userId, todo) {
+    client.query('DELETE FROM todos WHERE user_id = $1 AND todo = $2', [userId, todo], function(err, result) {
+        if(err) {
+            return console.error('error running query', err);
+        }
+        res.status(200).end();
+        done();
+    });
+}
